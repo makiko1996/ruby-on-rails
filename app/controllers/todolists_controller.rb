0d347e0d11cc0d4629c0c418b228b40ad844485e
makikoTakemori <richikaemay@gmail.com>
2021-11-07 06:50:37 +0000

    [update]3-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,27 @@
 class TodolistsController < ApplicationController
+  
   def new
+    # viewに渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
   end
+  
+  def create
+    # 1データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３トップ画面へリダイレクト
+    redirect_to'/top'
+  end
+
+  def index
+  end
+
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+ 
 end
