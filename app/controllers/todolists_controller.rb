d2fa11ec52b86e0b11564d7bd5907cb2bc08bc7e
makikoTakemori <richikaemay@gmail.com>
2021-11-09 05:16:37 +0000

    [update]1-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,12 +6,13 @@ class TodolistsController < ApplicationController
   end
   
   def create
-    # 1データを新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # ２データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # ３トップ画面へリダイレクト
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+     redirect_to todolist_path(@list.id)
+    else
+     render :new￥
+    end
+    
   end
 
   def index
@@ -21,12 +22,27 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolists_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body, :image)
   end
  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '21d086cf8820b59611fc4dde33ef0d85c0b302207c817c021ac92762636eaf0e65fb4809a94a82d9300332d2626821c64ae7bf1470c289a8a24ab315baadd326'
